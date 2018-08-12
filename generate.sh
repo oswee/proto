9999d3b2458a2418a251af09b805c1b939739240
oswee <dzintars.klavins@gmail.com>
2018-08-12 22:28:30 +0300

    Shipping implementation

--- generate.sh
+++ generate.sh
@@ -1,2 +1,3 @@
 protoc -I customer/ customer/customer.proto --go_out=plugins=grpc:customer/go
 protoc -I application/ application/application.proto --go_out=plugins=grpc:application/go
+protoc -I shipping/ shipping/shipping.proto --go_out=plugins=grpc:shipping/go
