11dc152a69763f94bad073be75df2bfed793da19
oswee <dzintars.klavins@gmail.com>
2018-08-14 22:48:05 +0300

    Implemented pageview logging

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
 protoc -I customer/ customer/customer.proto --go_out=plugins=grpc:customer/go
 protoc -I application/ application/application.proto --go_out=plugins=grpc:application/go
 protoc -I shipping/ shipping/shipping.proto --go_out=plugins=grpc:shipping/go
+protoc -I metric/ metric/metric.proto --go_out=plugins=grpc:metric/go
