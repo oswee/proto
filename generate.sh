e406f390381f5764a33d668336d5071a4dd2dedb
oswee <dzintars.klavins@gmail.com>
2018-08-17 14:14:25 +0300

    DeleteDeliveryOrder implementation

--- generate.sh
+++ generate.sh
@@ -2,3 +2,4 @@ protoc -I customer/ customer/customer.proto --go_out=plugins=grpc:customer/go
 protoc -I application/ application/application.proto --go_out=plugins=grpc:application/go
 protoc -I shipping/ shipping/shipping.proto --go_out=plugins=grpc:shipping/go
 protoc -I metric/ metric/metric.proto --go_out=plugins=grpc:metric/go
+protoc -I v1/ v1/os.proto --go_out=plugins=grpc:v1
