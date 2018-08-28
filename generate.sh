d858ebd5a1eac4cc0399543c0a5e97a3ae0f1233
oswee <dzintars.klavins@gmail.com>
2018-08-28 16:32:19 +0300

    Stub separation finished

--- generate.sh
+++ generate.sh
@@ -1,13 +1,5 @@
-protoc -I customer/ customer/customer.proto --go_out=plugins=grpc:customer/go
-protoc -I application/ application/application.proto --go_out=plugins=grpc:application/go
-protoc -I shipping/ shipping/shipping.proto --go_out=plugins=grpc:shipping/go
-protoc -I metric/ metric/metric.proto --go_out=plugins=grpc:metric/go
-protoc -I v1/ v1/os.proto --go_out=plugins=grpc:v1
-
-protoc -I routeguide/ routeguide/route_guide.proto --go_out=plugins=grpc:routeguide
-
-protoc --go_out=D:/Code/Go/src dms/v1/dms.proto
-protoc --go_out=D:/Code/Go/src app/v1/app.proto
-protoc --go_out=D:/Code/Go/src crm/v1/crm.proto
-protoc --go_out=D:/Code/Go/src metric/v1/metric.proto
-protoc --go_out=D:/Code/Go/src protobuf.proto
+protoc -I . -I dms/v1 --go_out=plugins=grpc:%GOPATH%/src/ dms/v1/dms.proto
+protoc -I . -I app/v1 --go_out=plugins=grpc:%GOPATH%/src/ app/v1/app.proto
+protoc -I . -I crm/v1 --go_out=plugins=grpc:%GOPATH%/src/ crm/v1/crm.proto
+protoc -I . -I metric/v1 --go_out=plugins=grpc:%GOPATH%/src/ metric/v1/metric.proto
+protoc -I . --go_out=plugins=grpc:%GOPATH%/src/ protobuf.proto
